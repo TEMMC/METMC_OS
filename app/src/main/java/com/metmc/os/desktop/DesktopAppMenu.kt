0fed33ae6d9b4a1807390866e19b230801a0ed37
TEMMC <temmc@users.noreply.github.com>
2026-08-29 11:59:55 +0200

    Fix ViewGroup import in DesktopAppMenu

--- app/src/main/java/com/metmc/os/desktop/DesktopAppMenu.kt
+++ app/src/main/java/com/metmc/os/desktop/DesktopAppMenu.kt
@@ -9,6 +9,7 @@ import android.graphics.drawable.GradientDrawable
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup
 import android.widget.*
 import android.widget.LinearLayout
 
