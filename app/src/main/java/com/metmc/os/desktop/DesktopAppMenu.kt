68cd929f943a31182b95f020fee1b9b13529cda6
TEMMC <temmc@users.noreply.github.com>
2026-09-05 09:56:57 +0200

    Fix missing Button import in DesktopAppMenu.kt

--- app/src/main/java/com/metmc/os/desktop/DesktopAppMenu.kt
+++ app/src/main/java/com/metmc/os/desktop/DesktopAppMenu.kt
@@ -9,6 +9,7 @@ import android.graphics.drawable.GradientDrawable
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.ScrollView
